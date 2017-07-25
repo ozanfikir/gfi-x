88cda6c4edac1d4e3cdac0d23f19529fd285da5d
Ozan Bora Fikir <ozan.fikir@aurea.com>
2017-07-25 17:36:36 +0200

    [GFIX-8] Feature 8 Implementation (#4)

---
+++ HelloGitFlow/HelloGitFlow/Feature8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGitFlow
+{
+    public class Feature8
+    {
+        public string Name { get; set; }
+
+        public int Index { get; set; }
+
+        public Feature8(string name)
+        {
+            Name = name;
+        }
+
+        public Feature8(string name, int index)
+        {
+            Name = name;
+            Index = index;
+        }
+    }
+}
