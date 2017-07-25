a3809ab6ca6011d9c5df5ce4a3f4ff084d553392
Ozan Bora Fikir <ozan.fikir@aurea.com>
2017-07-25 16:18:03 +0200

    [GFIX-3] Feature implemented.

---
+++ HelloGitFlow/HelloGitFlow/F1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGitFlow
+{
+    public class F1
+    {
+        /// <summary>
+        /// Gets or sets Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets Path
+        /// </summary>
+        public string Path { get; set; }
+    }
+}
