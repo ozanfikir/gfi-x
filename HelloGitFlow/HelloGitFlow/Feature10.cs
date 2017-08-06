7c2266a342fe6d2a5244ef63a581176571ba4ec8
Ozan Bora Fikir <ozan.fikir@aurea.com>
2017-08-06 16:20:28 +0200

    [GFIX-10] Feature 10 implementation.

---
+++ HelloGitFlow/HelloGitFlow/Feature10.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGitFlow
+{
+    public class Feature10
+    {
+        public string Name { get; set; }
+
+        public int Index { get; set; }
+
+        public Feature10()
+        {
+        }
+    }
+}
